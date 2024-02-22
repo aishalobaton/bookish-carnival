fe87933f390c0da7f5a7fb15d3e98a4ed8e65926
aishalobaton <116447601+aishalobaton@users.noreply.github.com>
2024-02-22 10:33:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,23 @@ st.image(image, caption='Parque')
 texto = st.text_input('Bienvenidos', 'Diviertanse')
 st.write('Hola', texto)
 
+st.subheader("Ahora presta atención a lo siguiente")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Primera columna")
+  st.write("Este tipo de parques son divertidos")
+  resp = st.checkbox('Claro que si')
+  if resp:
+    st.write('Asi es!')
+
+with col2:
+  st.subheader("Segunda columna")
+  modo= st.radio("Qué te gusta mas de los parques de diversiones?", ('Atracciones', 'Comida', 'Música'))
+  if modo == 'Atracciones':
+    st.write('Las atracciones son divertidas')
+  if modo == 'Comida':
+    st.write('La comida es deliciosa')
+  if modo == 'Música':
+    st.write('La música es buena')
